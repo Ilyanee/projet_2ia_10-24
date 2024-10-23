807afb0507d8223f108508fd4bee4fd02711d55d
Ilyanee <126757426+Ilyanee@users.noreply.github.com>
2024-10-23 17:27:05 +0200

    Add files via upload

---
+++ memory.java
@@ -0,0 +1,69 @@
+package calculatrice;
+
+public class memory implements CalculatorModelInterface {
+
+	public memory() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void add() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void substract() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void multiply() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void divide() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void opposite() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void push() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void pop() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void drop() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void swap() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void clear() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
