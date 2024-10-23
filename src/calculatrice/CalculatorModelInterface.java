38fe31832bec7d40baa727c9463dcf32a7df4ee7
Ilyanee <126757426+Ilyanee@users.noreply.github.com>
2024-10-23 17:23:51 +0200

    Add files via upload

---
+++ src/calculatrice/CalculatorModelInterface.java
@@ -0,0 +1,25 @@
+package calculatrice;
+
+public interface CalculatorModelInterface {
+	// Methodes interface à implémenter dans les classes si nécessaire
+
+	void add();
+	
+	void substract();
+	
+	void multiply();
+	
+	void divide();
+	
+	void opposite();
+	
+	void push();
+	
+	void pop();
+	
+	void drop();
+	
+	void swap();
+	
+	void clear();
+}
---
+++ src/calculatrice/accu.java
@@ -0,0 +1,75 @@
+package calculatrice;
+
+public class accu implements CalculatorModelInterface {
+	
+
+	public accu() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void add() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void substract() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void multiply() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void divide() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void opposite() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void push() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void pop() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void drop() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void swap() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void clear() {
+		// TODO Auto-generated method stub
+
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
