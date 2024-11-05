cc06f08835d45650435a45a2b0925e4a73d309d2
Ilyanee <126757426+Ilyanee@users.noreply.github.com>
2024-11-06 00:48:33 +0100

    Add files via upload

---
+++ CalculatriceMain.java
@@ -0,0 +1,28 @@
+package application;
+
+public class CalculatriceMain {
+
+	public static void main(String[] args) {
+		//Test
+		
+		Calculator calculatrice = new Calculator();
+		
+		try {
+			calculatrice.push(1);
+			calculatrice.opposite();
+			System.out.println("Pile :" + calculatrice.getPile());
+			
+			calculatrice.push(10);		
+			calculatrice.add();
+			System.out.println("Pile :" + calculatrice.getPile());
+			
+			calculatrice.clear();
+			System.out.println("Pile vidée :" + calculatrice.getPile());
+		}
+		
+		catch (Exception e) {
+			System.out.println("Erreur : " + e.getMessage());
+		}
+	}
+
+}
